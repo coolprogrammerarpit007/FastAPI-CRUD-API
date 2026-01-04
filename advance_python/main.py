3ffba10831e963ae498f0a7d5fc82ec00f1d6b80
Arpit <arpit.mishra@microprixs.in>
2026-01-04 18:56:33 +0530

    data-structures like list,sets,dictionary ,tuples and strings concept revision

---
+++ advance_python/main.py
@@ -0,0 +1,5 @@
+#  Lists in python are ordered,mutable and allowed duplicate elements
+# Tuples are ordered,Immutable and allows duplicate elements.
+# Dictionary:- Key-value pair,unordered,mutable
+# Sets:- are un-ordered,mutable and not allow duplicates.
+# strings:- ordered and im-mutable text representation of data
